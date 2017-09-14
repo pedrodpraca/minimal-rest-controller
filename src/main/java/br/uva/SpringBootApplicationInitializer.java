a67fc3a10aaae1e8a318b1424c9e4e21f1da4448
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2017-09-13 21:13:15 -0300

    Created branch clinica

--- src/main/java/br/uva/SpringBootApplicationInitializer.java
+++ src/main/java/br/uva/SpringBootApplicationInitializer.java
@@ -1,6 +1,5 @@
 package br.uva;
 
-import br.uva.services.gfx.TelaPrincipal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -19,30 +18,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class SpringBootApplicationInitializer extends SpringBootServletInitializer {
-
-    private static String url = null;
-    
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                TelaPrincipal telaPrincipal = new TelaPrincipal();
-                telaPrincipal.setVisible(true);
-
-                new Thread(new Runnable() {
-                    public void run() {
-                        ConfigurableApplicationContext context = SpringApplication.run(SpringBootApplicationInitializer.class, args);
-                        int port = ((TomcatEmbeddedServletContainer) ((AnnotationConfigEmbeddedWebApplicationContext) context).getEmbeddedServletContainer()).getPort();
-                        url = "http://localhost:" + port;
-                        telaPrincipal.readyState(url);
-                    }
-                }).start();
-            }
-        });
-    }
-
-    public static String getUrl() {
-        return url;
-    }
     
     @Override
     protected SpringApplicationBuilder
